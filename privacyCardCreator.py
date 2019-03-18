ef55e35cb7da139d4af5a3026706c7b4f451f484
Samuel <33304040+Sweetcharge@users.noreply.github.com>
2019-03-17 22:51:26 -0700

    Remove a single card and removing all cards finished
    Finished removing a single card and removing all the cards is almost done. Some testing will still be needed.

--- privacyCardCreator.py
+++ privacyCardCreator.py
@@ -1,5 +1,7 @@
 import requests
 
+cardListLength = 0
+
 def login():
     print("Logging in...")
     link = "https://privacy.com/auth/local"
@@ -21,17 +23,38 @@ def login():
 
     postRequest = requests.post(url=link, headers=headers, json=userInfo)
     request_status = postRequest.status_code
+    request_JSON = postRequest.json()
     if(request_status == 200):
-        request_JSON = postRequest.json()
         sessionToken = request_JSON["token"]
-        print("Logged in!")
+        print("Logged in!\n\n")
+        mainMenu(sessionToken)
+    else:
+        print(request_JSON)
 
-        cardName = input("Enter a card name: ")
-        cardLimit = input("Set a card limit: ")
+def mainMenu(token):
+        print("Welcome, your current cards are:\n")
+        showCurrentCards(token)
+        print("\n1. Create a new card\n2. Delete a card\n3. Delete all cards\n4. Exit")
+        userChoice = input("What would you like to do?: ")
 
-        createCard(sessionToken, cardName, cardLimit)
+        if(int(userChoice) == 1):
+            createCard(token)
+            mainMenu(token)
+        elif(int(userChoice) == 2):
+            deleteCard(token)
+            mainMenu(token)
+        elif(int(userChoice) == 3):
+            deleteAllCards(token)
+            mainMenu(token)
+        elif(int(userChoice) == 4):
+            print("Goodbye!")
+        else:
+            print("That's not a valid option")
+            mainMenu(token)
 
-def createCard(token, name, limit):
+def createCard(token):
+    cardName = input("Enter a card name: ")
+    cardLimit = input("Set a card limit: ")
     print("Attempting to create some cards...")
 
     link = "https://privacy.com/api/v1/card"
@@ -39,10 +62,10 @@ def createCard(token, name, limit):
     card_info = {
         'type': 'SINGLE_USE',
         'spendLimitDuration': 'FOREVER',
-        'memo': name,
+        'memo': cardName,
         'meta':{'hostname':""},
         'style': None,
-        'spendLimit': limit,
+        'spendLimit': cardLimit,
         'reloadable': 'false'
     }
 
@@ -59,9 +82,8 @@ def createCard(token, name, limit):
 
     postRequest = requests.post(url=link, headers=header, json=card_info)
     request_status = postRequest.status_code
+    request_JSON = postRequest.json()
     if(request_status == 200):
-        request_JSON = postRequest.json()
-
         cardName = request_JSON["card"]["memo"]
         cardStatus = request_JSON["accountState"]
         cardID = request_JSON["card"]["cardID"]
@@ -77,8 +99,16 @@ def createCard(token, name, limit):
         printCardInfo(cardName, cardStatus, cardID, cardNum, cardCVV, monthExp, yearExp, cardSpendLimit, cardType)
         createAnotherCard(token)
     else:
-        request_JSON = postRequest.json()
         print("ERROR: " + request_JSON["message"])
+
+def createAnotherCard(token):
+    createAnoter = input("Create another card? ")
+    if(createAnoter.lower() == "yes"):
+        print("\nWelcome, your current cards are:\n")
+        showCurrentCards(token)
+        createCard(token)
+    else:
+        print("Goodbye!")
 
 def printCardInfo(name, status, id, number, cvv, month, year, limit, cardType):
     print("\n"+name + " is " + status)
@@ -90,13 +120,97 @@ def printCardInfo(name, status, id, number, cvv, month, year, limit, cardType):
     print("Limit: "+str(limit))
     print("Card type: "+cardType)
 
-def createAnotherCard(sessionToken):
-    createAnoter = input("Create another card? ")
-    if(createAnoter == "Yes"):
-        cardName = input("Enter a card name: ")
-        cardLimit = input("Set a card limit: ")
+def deleteCard(token):
+    showCurrentCards(token)
+    cardToDelete = input("Which card would you like to delete? (i.e. 1): ")
+    cardID = getCardID(token, int(cardToDelete)-1)
+    removeCard(token, cardID)
 
-        createCard(sessionToken, cardName, cardLimit)
+def deleteAllCards(token):
+    userChoice = input("NOTE: This will remove all your currently open cards on Privacy, are you sure? ")
+    if(userChoice.lower() == "yes"):
+        print("Removing cards...")
+        print(cardListLength)
+        for i in range(cardListLength):
+            cardID = getCardID(token, i)
+            removeCard(token, cardID)
+        
+        print("Successfully removed all cards!\n")
+    else:
+        print("oof! That was a close one!")
+    
 
+def removeCard(token, id):
+    link = "https://privacy.com/api/v1/card/"+id+"/close"
+
+    newAuth = "Bearer "+token
+    newCookie = 'sessionID=3a809cfb-1176-4b43-a134-7cd1da832888; experiments=CiQ2MmFiZjUwNS0zN2Y0LTQyOTgtOWVjZi0yZWQ0MDY3MmY3YjkSJhIRc2hvdWxkU2tpcENvbmZpcm0qEXNob3dDbGFzc2ljRnVubmVs; ETag="ps26i5unssI="; token='+token
+
+    header = {
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
+		'content-type': 'application/json;charset=UTF-8',
+        'connection': 'keep-alive',
+        'authorization': newAuth,
+        'cookie': newCookie
+    }
+
+    postRequest = requests.post(url=link, headers=header)
+    request_status = postRequest.status_code
+    request_JSON = postRequest.json()
+    if(request_status == 200):
+        print("Card successfully deleted!\n")
+    else:
+        print("ERROR: "+request_JSON)
+
+def showCurrentCards(token):
+    global cardListLength
+    link = "https://privacy.com/api/v1/card/"
+
+    newAuth = "Bearer "+token
+    newCookie = 'sessionID=3a809cfb-1176-4b43-a134-7cd1da832888; experiments=CiQ2MmFiZjUwNS0zN2Y0LTQyOTgtOWVjZi0yZWQ0MDY3MmY3YjkSJhIRc2hvdWxkU2tpcENvbmZpcm0qEXNob3dDbGFzc2ljRnVubmVs; ETag="ps26i5unssI="; token='+token
+
+    header = {
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
+		'content-type': 'application/json;charset=UTF-8',
+        'connection': 'keep-alive',
+        'authorization': newAuth,
+        'cookie': newCookie
+    }
+
+    getRequest = requests.get(url=link, headers=header)
+    requestJSON = getRequest.json()
+    cardList = requestJSON["cardList"]
+    
+    if(cardListLength == 0):
+        print("No cards at the moment, you should make some.")
+    else: 
+        for cardIndex in range(len(cardList)):
+            if(cardList[cardIndex]["state"] == "OPEN"):
+                cardListLength = cardListLength + 1
+                openCardName = cardList[cardIndex]["memo"]
+                print(str(cardIndex+1)+") "+openCardName)
+
+def getCardID(token, target_cardIndex):
+    link = "https://privacy.com/api/v1/card/"
+
+    newAuth = "Bearer "+token
+    newCookie = 'sessionID=3a809cfb-1176-4b43-a134-7cd1da832888; experiments=CiQ2MmFiZjUwNS0zN2Y0LTQyOTgtOWVjZi0yZWQ0MDY3MmY3YjkSJhIRc2hvdWxkU2tpcENvbmZpcm0qEXNob3dDbGFzc2ljRnVubmVs; ETag="ps26i5unssI="; token='+token
+
+    header = {
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36',
+		'content-type': 'application/json;charset=UTF-8',
+        'connection': 'keep-alive',
+        'authorization': newAuth,
+        'cookie': newCookie
+    }
+
+    getRequest = requests.get(url=link, headers=header)
+    requestJSON = getRequest.json()
+    cardList = requestJSON["cardList"]
+
+    for cardIndex in range(len(cardList)):
+        if(cardList[cardIndex]["state"] == "OPEN" and target_cardIndex == cardIndex):
+            targetCardID = cardList[cardIndex]["cardID"]
+            return targetCardID
 
 login()    
